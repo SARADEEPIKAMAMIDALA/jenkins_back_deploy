dbd8d30820f90b9d18cfc647b2ad4b0727dcb134
SARADEEPIKAMAMIDALA <144265896+SARADEEPIKAMAMIDALA@users.noreply.github.com>
2025-10-15 20:03:53 +0530

    Update AuthController.java

--- src/main/java/com/klu/hospital/controller/AuthController.java
+++ src/main/java/com/klu/hospital/controller/AuthController.java
@@ -14,10 +14,7 @@ public class AuthController {
     public AuthController(UserService userService) {
         this.userService = userService;
     }
-@GetMapping("/home")
-public String Home(){
-    return "home this is";
-}
+
     @PostMapping("/signup")
     public ResponseEntity<String> registerUser(@RequestBody Map<String, String> request) {
         String username = request.get("username");
